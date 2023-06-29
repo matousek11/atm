f96b27233484046e9ea78674ed8962a8d6c795cc
Lukas Matousek <matousek1199@gmail.com>
2023-06-29 22:22:45 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Welcome in ATM." << std::endl;
+    std::cout << "Enter your pin:" << std::endl;
+    int pin = 0;
+    std::cin >> pin;
+    std::cout << pin;
+}
