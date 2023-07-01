76c1bfa3d75904ea3e0239c8a4a06b31f7823f67
Lukas Matousek <matousek1199@gmail.com>
2023-07-01 14:23:54 +0200

    Basic atm methods added

--- services/AtmLogic.cpp
+++ services/AtmLogic.cpp
@@ -1,8 +1,20 @@
 #include "AtmLogic.h"
 
-int AtmLogic::checkPin(int pin){
-    if(this->pin == pin)
+AtmLogic::AtmLogic()
+{
+    int newArray[6] = {100, 200, 500, 1000, 2000, 5000};
+    for (int i = 0; i < 6; i++)
+        this->moneyArray[i] = newArray[i];
+}
+
+int AtmLogic::checkPin(int pin)
+{
+    if (this->pin == pin)
         return 1;
     else
         return 0;
+}
+
+void AtmLogic::getVariantsOfReturn(int moneyToReturn)
+{
 }
